bd26bdc7dc1583f571e013341ef9ea2ae467cae2
Sravan Pappu <Shravan@Sravans-MacBook.local>
2017-12-23 22:28:46 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
 'use strict';
-
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
-
-// Imports dependencies and set up http server
-const 
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
-
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-  let body = req.body;
-
-  // Checks this is an event from a page subscription'use strict';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
 const 
