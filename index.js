1f91396b6895aa5a18e20e405b15b468f29eb5ee
Sravan Pappu <Shravan@Sravans-MacBook.local>
2017-12-23 22:34:55 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 'use strict';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
@@ -53,7 +54,6 @@ app.post('/webhook', (req, res) => {
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
   
-  //My Access Token
   const VERIFY_TOKEN = process.env.MY_ACCESS_TOKEN;
   
   // Parse params from the webhook verification request
@@ -160,102 +160,4 @@ function callSendAPI(sender_psid, response) {
       console.error("Unable to send message:" + err);
     }
   }); 
-}
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-  	// Gets the body of the webhook event
-  	let webhook_event = entry.messaging[0];
-  	console.log(webhook_event);
-
-  	// Get the sender PSID
-  	let sender_psid = webhook_event.sender.id;
-  	console.log('Sender PSID: ' + sender_psid);
-
-  	// Check if the event is a message or postback and
-  	// pass the event to the appropriate handler function
-  	if (webhook_event.message) {
-    	handleMessage(sender_psid, webhook_event.message);        
- 	 } else if (webhook_event.postback) {
-    	handlePostback(sender_psid, webhook_event.postback);
-  	}
-  
-
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
-
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.MY_ACCESS_TOKEN;
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
-});
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-	let response;
-
-  	// Check if the message contains text
-  	if (received_message.text) {    
-
-    	// Create the payload for a basic text message
-    	response = {
-      	"text": `You sent the message: "${received_message.text}". Now send me an image!`
-    }
-  }  
-  
-  // Sends the response message
-  callSendAPI(sender_psid, response); 
-
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  
-	// Construct the message body
- 	 let request_body = {
-    	"recipient": {
-      	"id": sender_psid
-    	},
-    	"message": response
-  	}
-
-  	
-
 }
