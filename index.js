12800dfa37c4753229478a2be133613c4cf302a6
Sravan Pappu <Shravan@Sravans-MacBook.local>
2017-12-23 22:40:20 -0600

    updated index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      "text": `welcome to AdventureWorks.You sent the message: "${received_message.text}".Please send the picture url`
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
